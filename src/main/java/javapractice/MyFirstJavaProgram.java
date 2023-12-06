eb30e36395879d088eadd0c63354749f1de5b4aa
tanuja <ktanuja996@gmail.com>
2023-12-06 09:42:11 +0530

    commit my first message

---
+++ src/main/java/javapractice/MyFirstJavaProgram.java
@@ -0,0 +1,19 @@
+package javapractice;
+
+public class MyFirstJavaProgram {
+	
+
+	public static void main(String[] args) {
+		int a=50;
+				
+	    int b=20;
+	
+	if(a>b) {
+	
+	System.out.println("a is less than of b");
+
+}
+	else {
+		System.out.println("a is greater than of b");
+	}}}	
+
